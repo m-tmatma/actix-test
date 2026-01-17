c7687fc0fafb68c94b866f93b96b4d241a1ba8bc
Masaru Tsuchiyama <m.tmatma@gmail.com>
2026-01-17 10:22:58 +0900

    html

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,19 @@ async fn user_info(path: web::Path<(String, u32)>) -> impl Responder {
     HttpResponse::Ok().body(format!("User: {}, ID: {}", username, id))
 }
 
+#[get("/html")]
+async fn render_html() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body("<h1>Hello, HTML!</h1>")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server");
     HttpServer::new(|| App::new()
         .service(hello)
+        .service(render_html)
         .route("/greet/{name}", web::get().to(greet_name))
         .route("/user/{username}/{id}", web::get().to(user_info)))
         .bind("127.0.0.1:8080")?
