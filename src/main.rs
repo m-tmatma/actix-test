7adda9dc608bbd2b00e20656a50d06d42cf9bddc
Masaru Tsuchiyama <m.tmatma@gmail.com>
2026-01-17 10:15:33 +0900

    add handler

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{web, get, App, HttpServer, Responder, HttpResponse};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     "Hello, world!"
 }
 
+async fn greet_name(name: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello, {}!", name))
+}
+
+async fn user_info(path: web::Path<(String, u32)>) -> impl Responder {
+    let (username, id) = path.into_inner();
+    HttpResponse::Ok().body(format!("User: {}, ID: {}", username, id))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server");
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new()
+        .service(hello)
+        .route("/greet/{name}", web::get().to(greet_name))
+        .route("/user/{username}/{id}", web::get().to(user_info)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
